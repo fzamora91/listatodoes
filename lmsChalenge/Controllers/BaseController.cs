c70cfc8a34319b88bb37eaba4368eb8beb5f5bf3
Felix Alejandro Zamora Sandoval <fzamora@versatecinc.com>
2024-09-09 18:38:50 -0600

    estructura inicial del proyecto implementando cqrs y mediatr

---
+++ lmsChalenge/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lmsChalenge.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private IMediator? _mediator;
+        protected IMediator Mediator => this._mediator;
+
+       /* public IActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
