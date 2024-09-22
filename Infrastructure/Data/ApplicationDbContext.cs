17e2c81d3c7f776504b379aac8b70434be0ad720
Felix Alejandro Zamora Sandoval <fzamora@versatecinc.com>
2024-09-21 19:33:38 -0600

    implementando sql en el proyecto de infraestructura

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IApplicationDbContext
+    {
+        public DbSet<Tarea> Tarea { get; set; }
+    }
+}
