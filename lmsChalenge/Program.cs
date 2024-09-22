e16154f1eef1bc9939d05157c7680e9d0c9c7f76
Felix Alejandro Zamora Sandoval <fzamora@versatecinc.com>
2024-09-21 19:32:10 -0600

    habilitando swagger en el servidor

--- lmsChalenge/Program.cs
+++ lmsChalenge/Program.cs
@@ -1,10 +1,26 @@
+using Application;
+using Infrastructure;
+using Shared.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+EngineContext.Create();
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+
+EngineContext.Current.Configure(app.Services);
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 // Configure the HTTP request pipeline.
 
