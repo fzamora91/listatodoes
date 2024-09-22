e571d3123ea67e420cf6acb437e7483f4f77270e
Felix Alejandro Zamora Sandoval <fzamora@versatecinc.com>
2024-09-21 19:29:18 -0600

    defiendo las acciones en el controlador de la entidad tarea

--- lmsChalenge/Controllers/BaseController.cs
+++ lmsChalenge/Controllers/BaseController.cs
@@ -1,16 +1,39 @@
-﻿using MediatR;
+﻿/*using Application.UseCases.Common.Results;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;*/
+
+using Application.UseCases.Common.Results;
+using AutoMapper;
+using Application;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Core;
+using Application.UseCases.Tareas.Queries.GetTareasPaginada;
 
 namespace lmsChalenge.Controllers
 {
     public class BaseController : ControllerBase
     {
         private IMediator? _mediator;
-        protected IMediator Mediator => this._mediator;
+        protected IMediator Mediator => this._mediator ??= EngineContext.Current.Resolve<IMediator>();
 
-       /* public IActionResult Index()
+        protected IMapper Mapper => EngineContext.Current.Resolve<IMapper>();
+
+        protected ActionResult FromResult<T>(Result<T> result) => result.ResultType switch
         {
-            return View();
-        }*/
+            ResultType.Ok => this.Ok(result),
+            ResultType.NotFound => this.NotFound(result),
+            ResultType.Created => this.Created(string.Empty, result),
+            _ => throw new Exception("Unhandled result"),
+        };
+
+        /*protected ActionResult FromResult<T>(PaginatedResult<T> result) => result.ResultType switch
+        {
+            ResultType.Ok => this.Ok(result),
+            ResultType.NotFound => this.NotFound(result),
+            ResultType.Created => this.Created(string.Empty, result),
+            _ => throw new Exception("Unhandled result"),
+        };*/
     }
 }
---
+++ lmsChalenge/Controllers/TareasController.cs
@@ -0,0 +1,88 @@
+﻿using Application.UseCases.Tareas.Commands.CompleteTarea;
+using Application.UseCases.Tareas.Commands.CreateTarea;
+using Application.UseCases.Tareas.Commands.RemoveTarea;
+using Application.UseCases.Tareas.Commands.UpdateTarea;
+using Application.UseCases.Tareas.Queries.GetTarea;
+using Application.UseCases.Tareas.Queries.GetTareas;
+using Application.UseCases.Tareas.Queries.GetTareasPaginada;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace lmsChalenge.Controllers
+{
+    public class TareasController : BaseController
+    {
+        [HttpPost]
+        [Route("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetTareasQuery();
+            var result = await this.Mediator.Send(query);
+            return this.FromResult(result);
+        }
+
+
+        [HttpPost]
+        [Route("Paginate")]
+        public async Task<IActionResult> Paginate([FromBody] TareaQueryModel model)
+        {
+            
+
+            var query = this.Mapper.Map<GetTareaPaginationQuery>(model);
+            var result = await this.Mediator.Send(query);
+            return Ok(result);
+        }
+
+
+
+        [HttpPost]
+        [Route("GetById")]
+        public async Task<IActionResult> GetById([FromBody] GetTareaQueryModel model)
+        {
+            var query = this.Mapper.Map<GetTareaQuery>(model);
+            var result = await this.Mediator.Send(query);
+            return this.FromResult(result);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateTarea([FromBody]CreateTareaCommandModel model)
+        {
+            var command = this.Mapper.Map<CreateTareaCommand>(model);
+            var result = await this.Mediator.Send(command);
+            return this.FromResult(result);
+        }
+
+        [HttpPost]
+        [Route("Completar")]
+        public async Task<IActionResult> MarcarTareaCompletada([FromBody] CompleteTareaCommandModel model)
+        {
+            var command = this.Mapper.Map<CompleteTareaCommand>(model);
+            var result = await this.Mediator.Send(command);
+            return this.FromResult(result);
+        }
+
+        [HttpPost]
+        [Route("Update")]
+        public async Task<IActionResult> UpdateTarea([FromBody] UpdateTareaCommandModel model)
+        {
+            var command = this.Mapper.Map<UpdateTareaCommand>(model);
+            var result = await this.Mediator.Send(command);
+            return this.FromResult(result);
+        }
+
+
+        [HttpDelete]
+        [Route("Delete")]
+        public async Task<IActionResult> DeleteTarea([FromBody] RemoveTareaCommandModel model)
+        {
+            var command = this.Mapper.Map<RemoveTareaCommand>(model);
+            var result = await this.Mediator.Send(command);
+            return this.FromResult(result);
+        }
+
+
+
+    }
+}
